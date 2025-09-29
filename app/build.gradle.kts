da1fa401f9a24f61791a21a10681221b2039506b
seanward1994 <seanward1994@gmail.com>
2025-09-29 04:45:05 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
 }
 dependencies {
   implementation(platform("androidx.compose:compose-bom:2024.09.03"))
+  implementation("com.google.android.material:material:1.12.0")
   implementation("androidx.core:core-ktx:1.13.1")
   implementation("androidx.activity:activity-compose:1.9.2")
   implementation("androidx.compose.ui:ui")
