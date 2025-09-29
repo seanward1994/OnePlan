805ea6f0fc547195e11f4066f75e606e84faa199
seanward1994 <seanward1994@gmail.com>
2025-09-29 10:47:19 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,7 @@ plugins {
   id("com.android.application") version "8.5.2" apply false
   id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
+plugins {
+    kotlin("jvm") version "1.9.24" apply false
+    id("org.jetbrains.compose") version "1.7.0" apply false
+}
